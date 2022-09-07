0e8c8a7780c2a94fe74874b034d54c291ba1e7df
oapm1996 <isc.oscarpina@gmail.com>
2022-09-06 21:52:19 -0700

    Adding Pacientes to the main state

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import Header from "./components/Header"
 import ListadoPacientes  from "./components/ListadoPacientes"
 
 function App() {
+  /* Creating a state called pacientes and a function called setPacientes. */
   const [pacientes, setPacientes] = useState ([]);
 
   return (
     <div className="container mx-auto mt-20">
-      <Header
-      />
       <Header/>
+
         <div className="mt-12 md:flex">
           <Formulario
+            /* Passing the pacientes state to the Formulario component. */
+            pacientes={pacientes}
+            /* Passing the setPacientes function to the Formulario component. */
             setPacientes = {setPacientes}
           />
           <ListadoPacientes/>
